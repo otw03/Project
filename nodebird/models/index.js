5ec29908b21a817cb646d3c71f87edb9ee482d53
otw03 <86701873+otw03@users.noreply.github.com>
2023-05-07 03:36:19 +0900

    node 실전프로젝트 클론코딩 SNS만들기 데이터베이 세팅

--- nodebird/models/index.js
+++ nodebird/models/index.js
@@ -1,36 +1,33 @@
-'use strict';
+const Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+const db = {};
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+db.sequelize = sequelize;
 
+const basename = path.basename(__filename);
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
+  .readdirSync(__dirname) // 현재 폴더의 모든 파일을 조회
+  .filter(file => { // 숨김 파일, index.js, js 확장자가 아닌 파일 필터링
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+  .forEach(file => { // 해당 파일의 모델 불러와서 init
+    const model = require(path.join(__dirname, file));
+    console.log(file, model.name);
     db[model.name] = model;
+    model.initiate(sequelize);
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach(modelName => { // associate 호출
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
